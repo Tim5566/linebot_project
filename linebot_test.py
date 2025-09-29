1d378c94bf0e0d68b0903bc813a41b867bbea747
Tim5566 <llomoll5566@gmail.com>
2025-09-29 16:30:26 +0800

    更新程式

--- linebot_test.py
+++ linebot_test.py
@@ -17,11 +17,6 @@ LINE_CHANNEL_SECRET = '0f27654b82ca9f667ac6c8eb37dc0a07'
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# 簡單首頁測試
-@app.route("/")
-def home():
-    return "LINE Bot Server 運行中！"
-
 # 保活用的 ping 路由
 @app.route("/ping")
 def ping():
@@ -44,43 +39,98 @@ def callback():
 def handle_message(event):
     user_text = event.message.text.strip()
 
-    # 查詢外資買賣超
-    reply_text = query_foreign_investor(user_text)
-
+    # 供查詢今日個股買賣超
+    reply_text = query_investor(user_text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
     )
 
-def query_foreign_investor(keyword):
-    #查詢今日外資買賣超
+# 供查詢今日個股買賣超
+def query_investor(keyword):
     today = datetime.datetime.now().strftime("%Y%m%d")
-    url = f"https://www.twse.com.tw/rwd/zh/fund/TWT38U?response=json&date={today}"
+    url_Foreign = f"https://www.twse.com.tw/rwd/zh/fund/TWT38U?response=json&date={20250926}"
+    url_Trust = f"https://www.twse.com.tw/rwd/zh/fund/TWT44U?response=json&date={20250926}"
+    url_Proprietary = f"https://www.twse.com.tw/rwd/zh/fund/TWT43U?response=json&date={20250926}"
+
     headers = {"User-Agent": "Mozilla/5.0"}  # 模擬瀏覽器，避免被 TWSE 拒絕
 
     try:
-        #res = requests.get(url, headers=headers)
-        res = requests.get(url, headers=headers, verify=False)
+        #外資
+        res = requests.get(url_Foreign, headers=headers, verify=True)
         data = res.json()
 
-        if data.get("stat") != "OK" or data.get("total", 0) == 0:
-            return "今天沒有交易資料，可能是休假或沒有外資交易。"
-
+        Foreign_text = None
         # data["data"] 格式: [證券代號, 證券名稱, 買進股數, 賣出股數, 買賣超股數]
         for row in data["data"]:
             stock_id, stock_name = row[1], row[2]
 
             if re.search(r'售|認購|認售', stock_name):
-                continue  # 跳過選擇權
+                continue #跳過選擇權
 
             if keyword == keyword in stock_id or keyword in stock_name:
-                return f"{stock_name.strip()} (今盤後)\n外資買賣超：{row[5]} 股"
+                Foreign_text = f"外資買賣超：{row[5]} 股"
+                break
+        
+        #投信
+        res = requests.get(url_Trust, headers=headers, verify=True)
+        data = res.json()
 
-        return f"找不到「{keyword}」的外資買賣超資料。"
+        Trust_text = None
+        # data["data"] 格式: [證券代號, 證券名稱, 買進股數, 賣出股數, 買賣超股數]
+        for row in data["data"]:
+            stock_id, stock_name = row[1], row[2]
+
+            if re.search(r'售|認購|認售', stock_name):
+                continue #跳過選擇權
+
+            if keyword == keyword in stock_id or keyword in stock_name:
+                Trust_text = f"投信買賣超：{row[5]} 股"
+                break
+        
+        #自營商
+        res = requests.get(url_Proprietary, headers=headers, verify=True)
+        data = res.json()
+
+        Proprietary_text = None
+        # data["data"] 格式: [證券代號, 證券名稱, 買進股數, 賣出股數, 買賣超股數]
+        for row in data["data"]:
+            stock_id, stock_name = row[1], row[2]
+
+            if re.search(r'售|認購|認售', stock_name):
+                continue #跳過選擇權
+
+            if keyword == keyword in stock_id or keyword in stock_name:
+                Proprietary_text = f"自營商買賣超：{row[5]} 股"
+                break
+
+        if Foreign_text or Trust_text or Proprietary_text:
+            reply = f"{keyword} (今盤後)\n"
+
+            #外資
+            if Foreign_text:
+                reply += Foreign_text + "\n"
+            else:
+                reply += "外資買賣超：暫未更新。" + "\n"
+
+            #投信
+            if Trust_text:
+                reply += Trust_text + "\n"
+            else:
+                reply += "投信買賣超：暫未更新。" + "\n"
+
+            #自營商
+            if Proprietary_text:
+                reply += Proprietary_text + "\n"
+            else:
+                reply += "自營商買賣超：暫未更新" + "\n"
+
+            return reply.strip()
+        else:
+            return f"找不到「{keyword}」的外資或投信買賣超資料。"
 
     except Exception as e:
-        logging.error("查詢發生錯誤: %s", e)
-        return f"查詢時發生錯誤：{e}"
+        return f"今日週休或連假未開盤"
 
 if __name__ == "__main__":
     # 使用 Render 提供的 PORT，並允許外部訪問
