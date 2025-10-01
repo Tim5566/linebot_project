52335e7cc1c493a713de3b13df2ee7723f2deae7
Tim5566 <llomoll5566@gmail.com>
2025-10-01 14:59:32 +0800

    更新程式

--- push_service.py
+++ push_service.py
@@ -2,6 +2,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from linebot.models import TextSendMessage
 
+import pytz
+
 def broadcast_job(line_bot_api):
     # 寫死訊息，或是這裡呼叫 query_investor 取得動態資料
     message = TextSendMessage(text="📢 盤後已更新資訊")
@@ -11,6 +13,6 @@ def start_scheduler(line_bot_api):
     scheduler = BackgroundScheduler()
     taiwan = pytz.timezone("Asia/Taipei")
     # 每天 18:00 推播一次（可依需求調整）
-    scheduler.add_job(lambda: broadcast_job(line_bot_api), 'cron', hour=14, minute=58, timezone=taiwan)
+    scheduler.add_job(lambda: broadcast_job(line_bot_api), 'cron', hour=15, minute=5, timezone=taiwan)
 
     scheduler.start()
