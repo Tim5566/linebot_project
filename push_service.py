08bc006426c2c4619f902628fadc4811a3e651a8
Tim5566 <llomoll5566@gmail.com>
2025-10-14 15:26:23 +0800

    更新

--- push_service.py
+++ push_service.py
@@ -41,7 +41,7 @@ def start_scheduler(line_bot_api):
     scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 0), 'cron', hour=15, minute=0, timezone=taiwan) #15:00
 
     # 盤後整體資訊更新時間
-    scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 1), 'cron', hour=15, minute=0, timezone=taiwan) #15:00
+    scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 1), 'cron', hour=15, minute=32, timezone=taiwan) #15:00
     
     # 三大法人個股買賣超更新時間
     scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 2), 'cron', hour=15, minute=0, timezone=taiwan) #15:00
@@ -49,7 +49,7 @@ def start_scheduler(line_bot_api):
     scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 4), 'cron', hour=16, minute=10, timezone=taiwan) #16:10
 
     ##大盤融資卷總金額統計
-    scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 5), 'cron', hour=20, minute=30, timezone=taiwan) #20:30
+    scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 5), 'cron', hour=21, minute=0, timezone=taiwan) #21:00
 
     # 個股借卷賣出
     scheduler.add_job(lambda: broadcast_post_inf(line_bot_api, 6), 'cron', hour=21, minute=30, timezone=taiwan) #21:30
